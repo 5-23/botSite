80f5d68e8d08c2bd4a5f6ae6a93a4ad1f607b629
SCRATCHER 5-23 <86705803+5-23@users.noreply.github.com>
2022-03-21 00:26:03 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("HI")
+let titleBox = document.getElementById("titleBox")
+let title = document.getElementById("title")
+let bot = document.getElementById("bot")
+
+
+
+function frame(){
+    if (titleBox.clientWidth < 530){
+        title.style.fontSize = `70px`
+        title.style.top = `3%`
+    }else if (titleBox.clientWidth < 787){
+        title.style.fontSize = `100px`
+        title.style.top = `1%`
+    }else{
+        title.style.fontSize = `140px`
+        title.style.top = `-2.5%`
+    }
+
+    if (window.scrollY > 500){
+        bot.style.animationDuration = `2s`
+        bot.style.animationFillMode = `both`
+        bot.style.animationName = `botIn`
+    }else{
+        bot.style.animationDuration = `2s`
+        bot.style.animationFillMode = `both`
+        bot.style.animationName = `botOut`
+    }
+    console.log(window.scrollY)
+
+    requestAnimationFrame(frame)
+}
+
+frame()
